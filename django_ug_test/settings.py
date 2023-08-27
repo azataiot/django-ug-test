2d6cfd0b432dd40dcd2b86cfc16d4fa59e3d8ed5
Azat <8280770+azataiot@users.noreply.github.com>
2023-08-27 16:29:28 +0300

    changing language settings.py

--- django_ug_test/settings.py
+++ django_ug_test/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,6 +109,11 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGES = (
+    ("en", _("English")),
+    ("ug", _("Uyghur")),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
